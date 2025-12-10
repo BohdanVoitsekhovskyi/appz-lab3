98fb350cf543b24ae5c395c8a842172664cfc554
BohdanVoitsekhovskyi <bohdan.voitsekhovskyi@gmail.com>
2025-12-10 21:45:32 +0200

    ненавиджу нулп

--- backend/libs/custom-loger-starter/src/main/java/custom/logger/starter/autoconfigurable/LoggerStarterAutoConfiguration.java
+++ backend/libs/custom-loger-starter/src/main/java/custom/logger/starter/autoconfigurable/LoggerStarterAutoConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @AutoConfiguration
 @EnableConfigurationProperties({LoggingProperties.class})
